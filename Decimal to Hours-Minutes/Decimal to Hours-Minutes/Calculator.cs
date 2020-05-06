8d99a7af12347e4c83b1d224765662cf79f308f6
Kyle Walton <kwwtno@outlook.com>
2020-05-05 23:22:52 -0500

    Initial Commit

---
+++ Decimal to Hours-Minutes/Decimal to Hours-Minutes/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal_to_Hours_Minutes
+{
+    class Calculator
+    {
+        //vars
+        static double userNumber, answer;
+        static int userRepeat;
+
+        //Program Logic
+        public void ProgramLogic()
+        {
+
+            do
+            {
+                userNumber = UserInput();
+                DoMath();
+
+                Console.WriteLine("Press any key to continue to enter another decimal and Esc to exit program.");
+                Console.ReadKey();
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                //exit the program
+                {
+
+                    System.Environment.Exit(0);
+
+                }
+
+            } while (true);
+
+        }
+
+
+        //MATH
+        private void DoMath()
+        {
+
+            var tsResult = TimeSpan.FromHours(userNumber); //Takes the decimal and converts to hours and minutes
+            Console.WriteLine("{0:D2} hour {1:D2} minutes", (int)tsResult.TotalHours, tsResult.Minutes); //Displayed the hours and minutes; Total hours is used to show only hour and not days.
+            Console.WriteLine("or \n{0:D2}:{1:D2}", (int)tsResult.TotalHours, tsResult.Minutes);
+
+        }
+
+        //Inquire the user to enter in a #'s
+        public static double UserInput()
+        {
+
+            Console.WriteLine("Insert Decimal.");
+            String value = Console.ReadLine();
+            double.TryParse(value, out double result);
+            return result;
+
+        }
+
+    }
+}
---
+++ Decimal to Hours-Minutes/Decimal to Hours-Minutes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal_to_Hours_Minutes
+{
+    class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            //Is a data type is an object, is kinda like a var that holds multiple vars
+            Calculator calculator = new Calculator();
+            calculator.ProgramLogic();
+
+        }
+
+    }
+}
