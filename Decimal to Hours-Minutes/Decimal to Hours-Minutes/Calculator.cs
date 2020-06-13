09b76fb1f26bbb786b11e3c12bd2ca3924a5561d
Kyle Walton <kwwtno@outlook.com>
2020-06-13 12:36:06 -0500

    Added input validation

--- Decimal to Hours-Minutes/Decimal to Hours-Minutes/Calculator.cs
+++ Decimal to Hours-Minutes/Decimal to Hours-Minutes/Calculator.cs
@@ -9,53 +9,63 @@ namespace Decimal_to_Hours_Minutes
     class Calculator
     {
         //vars
-        static double userNumber, answer;
-        static int userRepeat;
+        static double userNumber;
 
         //Program Logic
         public void ProgramLogic()
         {
-
             do
             {
-                userNumber = UserInput();
-                DoMath();
+                try
+                {
+                    userNumber = UserInput();
+                    DoMath();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }                                
 
-                Console.WriteLine("Press any key to continue to enter another decimal and Esc to exit program.");
+                Console.WriteLine("Press any key to continue or Esc to exit program.");
                 Console.ReadKey();
                 if (Console.ReadKey(true).Key == ConsoleKey.Escape)
                 //exit the program
                 {
-
                     System.Environment.Exit(0);
-
                 }
 
-            } while (true);
+                //Writes a new line as to not have the user's input run into the Insert Decimal text
+                Console.WriteLine("");
 
+            } while (true);
         }
 
 
         //MATH
         private void DoMath()
         {
-
-            var tsResult = TimeSpan.FromHours(userNumber); //Takes the decimal and converts to hours and minutes
-            Console.WriteLine("{0:D2} hour {1:D2} minutes", (int)tsResult.TotalHours, tsResult.Minutes); //Displayed the hours and minutes; Total hours is used to show only hour and not days.
+            //Takes the decimal and converts to hours and minutes
+            var tsResult = TimeSpan.FromHours(userNumber);
+            //Displayed the hours and minutes; Total hours is used to show only hour and not days.
+            Console.WriteLine("{0:D2} hour {1:D2} minutes", (int)tsResult.TotalHours, tsResult.Minutes); 
             Console.WriteLine("or \n{0:D2}:{1:D2}", (int)tsResult.TotalHours, tsResult.Minutes);
-
         }
 
         //Inquire the user to enter in a #'s
         public static double UserInput()
         {
-
             Console.WriteLine("Insert Decimal.");
-            String value = Console.ReadLine();
-            double.TryParse(value, out double result);
-            return result;
-
+            String userinput = Console.ReadLine();
+            try
+            {
+                return Convert.ToDouble(userinput);
+            }
+            catch
+            {
+                //Catches bugs or issues. 
+                //In this case it would detect non number data.
+                throw new Exception("Please input numbers only.");
+            }
         }
-
     }
 }
